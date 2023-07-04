5f63a153792cb0fb3be38f9fb9c5221494b72368
Filip Seman <filip.seman@protonmail.com>
2023-07-04 15:01:33 +0200

    feat: categorize services, add section ids, inline svg logo

--- _config.ts
+++ _config.ts
@@ -3,6 +3,7 @@ import lume from "lume/mod.ts";
 import esbuild from "lume/plugins/esbuild.ts";
 import lightningCss from "lume/plugins/lightningcss.ts";
 import sitemap from "lume/plugins/sitemap.ts";
+import inline from "lume/plugins/inline.ts";
 
 const site = lume({
 	location: new URL("http://medbi.sk"),
@@ -10,8 +11,9 @@ const site = lume({
 
 site
 	.copy("assets")
-	/** needs location config, else localhost is default the domain name */
+	/** needs location config, else localhost is the domain name */
 	.use(sitemap())
+	.use(inline())
 	.use(esbuild({
 		extensions: [".js", ".ts"],
 	}))
