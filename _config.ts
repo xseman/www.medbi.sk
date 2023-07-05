0e8c47ef690127cb571d0f26b7ada08386be8b07
Filip Seman <filip.seman@protonmail.com>
2023-07-05 08:07:15 +0200

    feat: add minify html plugin

--- _config.ts
+++ _config.ts
@@ -1,9 +1,10 @@
 import lume from "lume/mod.ts";
 
 import esbuild from "lume/plugins/esbuild.ts";
-import lightningCss from "lume/plugins/lightningcss.ts";
 import sitemap from "lume/plugins/sitemap.ts";
 import inline from "lume/plugins/inline.ts";
+import minifyHTML from "lume/plugins/minify_html.ts";
+import lightningCss from "lume/plugins/lightningcss.ts";
 
 const site = lume({
 	location: new URL("http://medbi.sk"),
@@ -14,9 +15,12 @@ site
 	/** needs location config, else localhost is the domain name */
 	.use(sitemap())
 	.use(inline())
+	.use(minifyHTML())
 	.use(esbuild({
 		extensions: [".js", ".ts"],
+		options: { treeShaking: true },
 	}))
+	/** also automaticly copy static files based on css import */
 	.use(lightningCss({
 		options: { minify: true },
 	}));
