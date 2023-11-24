60c975de54af33b6be5c162b71803943ef3c700b
Filip Seman <filip.seman@protonmail.com>
2023-11-24 10:25:41 +0100

    chore: update map zoom level

--- kontakt/index.ts
+++ kontakt/index.ts
@@ -2,12 +2,12 @@ import leaflet from "https://esm.sh/leaflet@1.9.3";
 
 const map = leaflet
 	.map("map")
-	.setView([48.33122, 17.30852], 18);
+	.setView([48.33122, 17.30852], 15);
 
-const tiles = leaflet
+const _tiles = leaflet
 	.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png")
 	.addTo(map);
 
-const marker = leaflet
+const _marker = leaflet
 	.marker([48.33125, 17.30853])
 	.addTo(map);
