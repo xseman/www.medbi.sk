ac868167edbc52c4b6d688094dba2dd73a250517
Filip Seman <filip.seman@protonmail.com>
2024-01-01 17:48:59 +0100

    chore: update lume, add dprint, switch water.css to tacit.css

--- _config.ts
+++ _config.ts
@@ -1,36 +1,41 @@
 import lume from "lume/mod.ts";
 
 import esbuild from "lume/plugins/esbuild.ts";
-import sitemap from "lume/plugins/sitemap.ts";
 import inline from "lume/plugins/inline.ts";
-import minifyHTML from "lume/plugins/minify_html.ts";
 import lightningCss from "lume/plugins/lightningcss.ts";
 import metas from "lume/plugins/metas.ts";
+import minifyHTML from "lume/plugins/minify_html.ts";
+import nunjucks from "lume/plugins/nunjucks.ts";
+import sitemap from "lume/plugins/sitemap.ts";
 
 const site = lume({
 	/** required for sitemap() */
 	location: new URL("http://medbi.sk"),
 });
 
-site
-	.copy("assets")
+site.copy("assets")
 	.copy("404.html")
 	.remoteFile(
-		"./_includes/water.css",
-		"https://esm.sh/water.css@2.1.1/out/water.css",
+		"./_includes/tacit.css",
+		"https://esm.sh/tacit-css/dist/tacit-css-1.6.0.css"
 	)
+	.use(nunjucks())
 	.use(sitemap())
 	.use(metas())
 	.use(inline())
 	.use(minifyHTML())
-	.use(esbuild({
-		extensions: [".js", ".ts"],
-		options: { treeShaking: true, minify: true },
-	}))
+	.use(
+		esbuild({
+			extensions: [".js", ".ts"],
+			options: { treeShaking: true, minify: true },
+		})
+	)
 	/** also automaticly copy static files based on css import */
-	.use(lightningCss({
-		// includes: "./_includes",
-		options: { minify: true },
-	}));
+	.use(
+		lightningCss({
+			// includes: "./_includes",
+			options: { minify: true },
+		})
+	);
 
 export default site;
